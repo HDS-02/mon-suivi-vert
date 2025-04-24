555a742b78d67b0b91628043979269dd394aba92
HDS-02 <contact@histoiredunsouvenir.fr>
2025-04-24 20:43:50 +0200

    Frontend amélioré visible

---
+++ server.js
@@ -0,0 +1,22 @@
+
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import cors from "cors";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, "client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/dist/index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`✔ Serveur lancé sur http://localhost:${port}`);
+});
